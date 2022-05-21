0ddc544faced9b4988a0cb35fa691e69e9c48cf9
guss-renteria <gusstavo.renteria.g@gmail.com>
2022-05-21 20:43:06 +0000

    primer 'commit'

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Suspense, lazy } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Loading from './views/Loading/Loading.view'
+import NotFound from './views/NotFound/NotFound.view'
+
+const InitView = lazy(() => import('./views/Init/Init.view'))
+
+function App() {
+  return (
+    <Router>
+      <Suspense fallback={ <Loading /> }>
+        <Routes>
+          <Route path='/' element={ <InitView /> }/>
+          <Route path='*' element={ <NotFound /> }/>
+        </Routes>
+      </Suspense>
+    </Router>
+  )
+}
+
+export default App
